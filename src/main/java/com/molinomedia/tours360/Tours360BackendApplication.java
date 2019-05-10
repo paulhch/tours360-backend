fa5db2c026372c1f49b4bf4a2e472c26fac8e9c8
paul <paulhch@gmail.com>
2019-05-09 23:55:27 -0500

    Configuración de mysql

---
+++ src/main/java/com/molinomedia/tours360/Tours360BackendApplication.java
@@ -0,0 +1,13 @@
+package com.molinomedia.tours360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tours360BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tours360BackendApplication.class, args);
+    }
+
+}
